d8b6986b7cbc41ee8a1e2a80162a78c913b9c5bd
rob134 <robson_scavazzini@hotmail.com>
2025-02-06 16:52:52 -0300

    Initial commit: Adicionando projeto backend

---
+++ transferencias/src/main/java/com/examplo/transferencias/TransferenciasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.examplo.transferencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferenciasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferenciasBackendApplication.class, args);
+	}
+
+}
---
+++ transferencias/src/test/java/com/examplo/transferencias/TransferenciasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplo.transferencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferenciasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
